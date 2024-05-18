126284ee90b9738bd29faea0392f93252ae2e318
Mirza Niaz Morshed <129221097+niazmorshed1@users.noreply.github.com>
2024-05-18 16:36:28 +0600

    Update pro.js

--- pro.js
+++ pro.js
@@ -14,69 +14,65 @@ searchBtn.addEventListener("click", () => {
   }
 });
 
-const searchMeals = async (searchVal) => {
-  try {
-    const fetchMeals = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${searchVal}`);
-    const res = await fetchMeals.json();
-    mealsDirectory.innerHTML = "";
-    if (res.meals) {
-      res.meals.forEach((meal) => {
-        const itemCard = document.createElement("div");
-        itemCard.classList.add("itemCard");
-        itemCard.innerHTML = `
-          <div class="innerItem">
-            <img class="itemImg" src="${meal.strMealThumb}" alt="">
-            <div>
-              <h2>${meal.strMeal}</h2>
-              <p class="tag">${meal.strTags || "No tags"}</p>
-              <p class="details">${meal.strInstructions.slice(0, 50)}...</p>
-            </div>
-          </div>`;
-        itemCard.addEventListener("click", () => modalOpen(meal.idMeal));
-        mealsDirectory.appendChild(itemCard);
-      });
-    } else {
-      mealsDirectory.innerHTML = "<p style='text-align:center;font-size:20px;'>Not Found</p>";
-    }
-  } catch (err) {
-    console.log("Error:", err);
-  }
-};
+function searchMeals(searchVal) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${searchVal}`)
+    .then(response => response.json())
+    .then(res => {
+      mealsDirectory.innerHTML = "";
+      if (res.meals) {
+        res.meals.forEach((meal) => {
+          const itemCard = document.createElement("div");
+          itemCard.classList.add("itemCard");
+          itemCard.innerHTML = `
+            <div class="innerItem">
+              <img class="itemImg" src="${meal.strMealThumb}" alt="">
+              <div>
+                <h2>${meal.strMeal}</h2>
+                <p class="tag">${meal.strTags || "No tags"}</p>
+              </div>
+            </div>`;
+          itemCard.addEventListener("click", () => modalOpen(meal.idMeal));
+          mealsDirectory.appendChild(itemCard);
+        });
+      } else {
+        mealsDirectory.innerHTML = "<p style='text-align:center;font-size:20px;'>Not Found</p>";
+      }
+    })
+    .catch(err => console.log("Error:", err));
+}
 
-const modalOpen = async (itemID) => {
+function modalOpen(itemID) {
   const mainModal = document.getElementById("modalMain");
   mainModal.innerHTML = ""; 
   mainModal.style.display = "block";
 
-  try {
-    const res = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${itemID}`);
-    const resdata = await res.json();
-    const info = resdata.meals[0];
-    
-    const modalContent = document.createElement("div");
-    modalContent.classList.add("modal");
-    modalContent.innerHTML = `
-      <div class="img-icon">
-        <div class="close-icon">
-          <i class="fa-solid fa-circle-xmark" onclick="closeToggle()"></i>
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${itemID}`)
+    .then(response => response.json())
+    .then(resdata => {
+      const info = resdata.meals[0];
+      const modalContent = document.createElement("div");
+      modalContent.classList.add("modal");
+      modalContent.innerHTML = `
+        <div class="img-icon">
+          <div class="close-icon" onclick="closeToggle()">
+            <i class="fa-solid fa-circle-xmark"></i>
+          </div>
+          <img src="${info.strMealThumb}" alt="">
         </div>
-        <img src="${info.strMealThumb}" alt="">
-      </div>
-      <h2>${info.strMeal}</h2>
-      <p class="tag">${info.strTags || "No tags"}</p>
-      <p class="details">${info.strInstructions}</p>
-      <h4>Ingredients</h4>
-      <ul>
-        ${Object.keys(info)
-          .filter(key => key.startsWith("strIngredient") && info[key])
-          .map(key => `<li>${info[key]}</li>`).join("")}
-      </ul>`;
-    mainModal.appendChild(modalContent);
-  } catch (err) {
-    console.log("Error:", err);
-  }
-};
+        <h2>${info.strMeal}</h2>
+        <p class="tag">${info.strTags || "No tags"}</p>
+        <p class="details">${info.strInstructions}</p>
+        <h4>Ingredients</h4>
+        <ul>
+          ${Object.keys(info)
+            .filter(key => key.startsWith("strIngredient") && info[key])
+            .map(key => `<li>${info[key]}</li>`).join("")}
+        </ul>`;
+      mainModal.appendChild(modalContent);
+    })
+    .catch(err => console.log("Error:", err));
+}
 
-const closeToggle = () => {
+function closeToggle() {
   modalToggle.style.display = "none";
-};
+}
